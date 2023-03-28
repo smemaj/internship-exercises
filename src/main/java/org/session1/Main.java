4a4f442038b8c8ae3855d183a08f237f9a69f122
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-03-29 01:02:08 +0200

    cleanup

--- src/main/java/org/session1/Main.java
+++ src/main/java/org/session1/Main.java
@@ -11,11 +11,6 @@ public class Main {
         Question q2 = new Question(2, "question 2");
         Question q3 = new Question(3, "question 3");
 
-        System.out.println(q1 + "\n");
-        System.out.println(q2 + "\n");
-        System.out.println(q3 + "\n");
-
-//        Answer a1 = Answer.AGREE;
 
         Candidate c1 = new Candidate();
         c1.setFirstName("first1");
@@ -23,10 +18,7 @@ public class Main {
         c1.setEmail("flast1@gmail.com");
         c1.setPhone("123456789");
 
-        System.out.println(c1 + "\n");
-
         Candidate c2 = new Candidate("first2", "last2", "flast2@gmail.com", "987654321");
-        System.out.println(c2 + "\n");
 
         List<Candidate> candidateList = new ArrayList<>();
         candidateList.add(c1);
@@ -37,72 +29,12 @@ public class Main {
         questions.add(q2);
         questions.add(q3);
 
-//        Set<Question> set = new HashSet<Question>(questions);
-//
-//        System.out.println("list of questions "+questions + "\n");
-//        System.out.println("set of questions "+set + "\n");
-//        System.out.println("are they equal "+q1.equals(q2));
-//        List<Integer> listeProve = new ArrayList<>();
-//        listeProve.add(1);
-//        listeProve.add(1);
-//        listeProve.add(2);
-//
-//        System.out.println(listeProve);
-//        Set<Integer> set2 = new HashSet<Integer>(listeProve);
-//        System.out.println(set2);
-
         Survey survey = new Survey("Math Survey", "Mathematics", "A short survey about mathematics.", questions);
 
-        System.out.println(survey + "\n");
 
         System.out.println(survey.validateSurvey());
         List<SurveyResult> surveyResultList = new ArrayList<>();
-//        Map<Question, Answer> result = new HashMap<>();
-//        result.put(q1, Answer.AGREE);
-//        result.put(q2, Answer.AGREE);
-//        result.put(q3, Answer.DISAGREE);
-//
-//        Map<Question, Answer> result2 = new HashMap<>();
-//        result2.put(q1, Answer.SLIGHLTY_AGREE);
-//        result2.put(q2, Answer.DISAGREE);
-//        result2.put(q3,Answer.SLIGHTLY_DISAGREE);
-//
-//        SurveyResult surveyResult1 = new SurveyResult(survey, c1, result);
-//        SurveyResult surveyResult2 = new SurveyResult(survey, c2, result2);
-//
-//        System.out.println(surveyResult1);
-//        System.out.println(surveyResult2);
-//
-//        System.out.println(surveyResult1.candidateResults(c1));
-//        System.out.println(surveyResult2.candidateResults(c2));
 
-
-//
-//
-//        Map<Question, Answer> result = new HashMap<>();
-//
-//
-//        Scanner scanner = new Scanner(System.in);
-
-//        System.out.println("Enter answer: ");
-//
-//        String answer = scanner.nextLine();
-//
-//        System.out.println("answer is: "+ Answer.AGREE.name());
-//
-//        if (answer.equals(Answer.AGREE.name())){
-//            result.put(q1, Answer.AGREE);
-//            result.put(q2, Answer.AGREE);
-//            System.out.println(true);
-//        }else {
-//            System.out.println(false);
-//        }
-//        System.out.println(result + "\n");
-//
-//        SurveyResult surveyResult1 = new SurveyResult(survey, c1, result);
-//        SurveyResult surveyResult2 = new SurveyResult(survey, c2, result);
-//        System.out.println(surveyResult1);
-//        System.out.println(surveyResult2);
         System.out.println("Welcome to Survey Application...");
 
         while (true){
@@ -123,7 +55,6 @@ public class Main {
                     System.out.println("Valid candidate. Please continue to the survey");
                     candidate=c;
                     System.out.println("Please choose an answer for each question.");
-                    System.out.println("answer name: "+ Answer.SLIGHTLY_AGREE.name().replace('_',' '));
                     Map<Question, Answer> questionAnswerMap = new HashMap<>();
 
 
@@ -169,4 +100,29 @@ public class Main {
 
     }
 
+//    public void surveyResults (List<SurveyResult> surveyResultList) {
+//        for(SurveyResult surveyResult: surveyResultList){
+//            List<Integer> counter = List.of(0,0,0,0);
+//            for (Map.Entry<Question, Answer> entry : surveyResult.getResults().entrySet()) {
+//                Question question = entry.getKey();
+//                Answer answer = entry.getValue();
+//
+//                if(answer.name().equals(Answer.AGREE.name())){
+//                    Integer var =counter.get(0);
+//                    counter.set(0, var++);
+//                }else if(answer.name().equals(Answer.SLIGHTLY_AGREE.name())){
+//                    Integer var =counter.get(1);
+//                    counter.set(1, var++);
+//                }else if(answer.name().equals(Answer.SLIGHTLY_DISAGREE.name())){
+//                    Integer var =counter.get(2);
+//                    counter.set(2, var++);
+//                }else if(answer.name().equals(Answer.DISAGREE.name())){
+//                    Integer var =counter.get(3);
+//                    counter.set(3, var++);
+//                }
+//
+//            }
+//
+//        }
+//    }
 }
